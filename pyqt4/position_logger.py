d54307a9c83f9fbe6cc16be33f8c9e96b3417aaa
John Thornton <bjt128@gmail.com>
2018-09-12 04:55:16 -0500

    test for linuxcnc is running
    Signed-off-by: John Thornton <bjt128@gmail.com>

--- pyqt4/position_logger.py
+++ pyqt4/position_logger.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python
-import os, sys
-from PyQt4 import QtGui, uic
 
-class MyWindow(QtGui.QMainWindow):
+import os, sys
+import linuxcnc
+from PyQt4 import uic
+from PyQt4.QtGui import *
+
+
+class MyWindow(QMainWindow):
 	def __init__(self):
 		super(MyWindow, self).__init__()
+		self.s = linuxcnc.stat() # create a connection to the status channel
+		try: # make sure linuxcnc is running
+			self.s.poll()
+		except linuxcnc.error:
+			msg = QMessageBox()
+			msg.setIcon(QMessageBox.Critical)
+			msg.setWindowTitle('Error')
+			msg.setText('LinuxCNC is not running')
+			msg.setInformativeText('Start LinuxCNC first.')
+			msg.setStandardButtons(QMessageBox.Ok)
+			msg.exec_()
+			exit()
 		projectPath = os.path.abspath(os.path.dirname(sys.argv[0]))
 		uic.loadUi(os.path.join(projectPath, 'position_logger.ui'), self)
 		self.show()
@@ -13,14 +29,13 @@ class MyWindow(QtGui.QMainWindow):
 		self.setupConnections()
 
 	def setupConnections(self):
-		self.actionExit.triggered.connect(self.exit())
+		self.actionExit.triggered.connect(self.exit)
 
 	def exit(self):
-		print 'exit'
 		exit()
 
 
 if __name__ == '__main__':
-	app = QtGui.QApplication(sys.argv)
+	app = QApplication(sys.argv)
 	window = MyWindow()
 	sys.exit(app.exec_())
