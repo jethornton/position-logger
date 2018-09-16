593d6278ee97161f4c9eaca0ad332b1997ec43d2
jethornton <jthornton@gnipsel.com>
2018-09-16 08:11:56 -0500

    get display to work
    Signed-off-by: jethornton <jthornton@gnipsel.com>

--- pyqt4/position_logger.py
+++ pyqt4/position_logger.py
@@ -2,8 +2,15 @@
 
 import os, sys
 import linuxcnc
+# This needs to be done once at the beginning of your program,
+# before the other PyQt modules are imported, and will ensure that
+# a QVariant is never returned by any Qt API.
+import sip
+sip.setapi('QVariant', 2)
+
 from PyQt4 import uic
 from PyQt4.QtGui import *
+from PyQt4.QtCore import QTimer
 
 
 class MyWindow(QMainWindow):
@@ -23,25 +30,43 @@ class MyWindow(QMainWindow):
 			exit()
 		projectPath = os.path.abspath(os.path.dirname(sys.argv[0]))
 		uic.loadUi(os.path.join(projectPath, 'position_logger.ui'), self)
+		self.axes = [(i) for i in range(9)if self.s.axis_mask & (1<<i)]
 		self.setupGUI()
 		self.setupConnections()
+		self.timer = QTimer()
+		self.timer.timeout.connect(self.update)
+		self.timer.start(100)
 		self.show()
+
 
 	def setupConnections(self):
 		self.actionExit.triggered.connect(self.exit)
 
+
 	def setupGUI(self):
-		self.positionCB.addItem('Reletive', 'positionReletive')
-		self.positionCB.addItem('Machine', 'positionMachine')
+		self.positionCB.addItem('Relative', 'relative')
+		self.positionCB.addItem('Absolute', 'absolute')
 
 		# check axes that are and disable axes that are not
-		axes = [(i) for i in range(9)if self.s.axis_mask & (1<<i)]
 		for i in range(9):
-			getattr(self, 'axisCB_' + str(i)).setChecked(i in axes)
+			getattr(self, 'axisCB_' + str(i)).setChecked(i in self.axes)
+
+	def update(self):
+		self.s.poll()
+		if self.positionCB.itemData(self.positionCB.currentIndex()) == 'relative':
+			# sum the offsets with a negative sign
+			offsets = tuple(-sum(i) for i in zip(self.s.g5x_offset,self.s.g92_offset))
+			display = tuple(sum(i) for i in zip(offsets,self.s.actual_position))
+			#print offsets
+			#print self.s.actual_position
+			#print display
+		else:
+			display = self.s.actual_position
+		for i in self.axes:
+			getattr(self, 'positionLB_' + str(i)).setText('{0:0.{1}f}'.format(display[i], self.precisionSB.value()))
 
 	def exit(self):
 		exit()
-
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
