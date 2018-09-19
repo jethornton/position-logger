10f1752cd01d00a09cf38bb5f0c5ec05943c04ff
jethornton <jthornton@gnipsel.com>
2018-09-19 10:11:20 -0500

    give setup.py a stab
    Signed-off-by: jethornton <jthornton@gnipsel.com>

--- setup.py
+++ setup.py
@@ -1,42 +1,23 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+long_description = fh.read()
 
 setuptools.setup(
-    name="example_pkg",
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
-
-from setuptools import setup
-
-setup(
-    # Needed to silence warnings (and to be a worthwhile package)
-    name='Measurements',
-    url='https://github.com/jethornton/position-logger',
-    author='John Thornton',
-    author_email='bjt128@gmail.com',
-    # Needed to actually package something
-    packages=['position_logger'],
-    # Needed for dependencies
-    install_requires=['numpy'],
-    # *strongly* suggested for sharing
-    version='0.1',
-    # The license can be anything you like
-    license='MIT',
-    description='LinuxCNC postion logger',
-    # We will also need a readme eventually (there will be a warning)
-    long_description=open('README.md').read(),
+	name="position_logger",
+	version="0.0.1",
+	author="John Thornton",
+	author_email="bjt128@gmail.com",
+	description="LinuxCNC Position Logger",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/jethornton/position-logger",
+	packages=setuptools.find_packages(),
+	install_requires=['python-pyqt'],
+	classifiers=[
+		"Development Status :: 3 - Alpha",
+		"Programming Language :: Python :: 2.7",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: POSIX :: Linux",
+	],
 )
